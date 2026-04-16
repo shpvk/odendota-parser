2bd1f762b2863729f06e322909c90be3b3bca4bb
shpvk <holotovegor99@gmail.com>
2026-04-16 20:00:38 +0300

    feat: Add Service logic

--- protracker-parser/Services/ParserService.cs
+++ protracker-parser/Services/ParserService.cs
@@ -1,6 +1,16 @@
-﻿namespace protracker_parser.Services
+﻿using protracker_parser.Models;
+
+namespace protracker_parser.Services
 {
     public class ParserService
     {
+        public (int? gamesAmount, int? winrate, string? Error) Process(string inputText)
+        {
+            if (string.IsNullOrWhiteSpace(inputText))
+                return (null, null, "Incorrect input string");
+
+            return (10000, 100, null); // Temporary plug
+        }
+
     }
 }
